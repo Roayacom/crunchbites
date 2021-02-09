8221d99eaf63848fe53913f327d03072112b3f77
Roayacom <77238762+Roayacom@users.noreply.github.com>
2021-02-09 20:08:03 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+
+    extend: {
+
+      colors:{
+    
+        orangishred:{
+          200:'#D16161',
+          DEFAULT:'#BE3737',
+          800:'#7E2525',
+        },
+
+browngray:{
+        200:'#5F5C58',
+        DEFAULT:'#353331',
+        800:'#0B0A0A',
+      },
+
+
+      beigeorange:{
+        200:'#F8E3C9',
+        DEFAULT:'#EDB672',
+        800:'#E59738',
+      }
+
+      },
+
+      
+  
+
+
+
+
+    
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
